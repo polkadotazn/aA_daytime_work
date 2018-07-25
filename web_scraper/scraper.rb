3678746c67672ab2d77c92d58fccdb54324fe828
Louisa Xue <xuelex@gmail.com>
2018-07-25 13:31:19 -0700

    Find duplicate entries

--- web_scraper/scraper.rb
+++ web_scraper/scraper.rb
@@ -4,43 +4,47 @@ require 'byebug'
 
 def scraper
 
-  url = "https://www.indeed.com/jobs?q=software+engineer+javascript&l=San+Francisco,+CA&explvl=entry_level&start=0"
-  unparsed_page = HTTParty.get(url)
-  parsed_page = Nokogiri::HTML(unparsed_page)
-  job_listings = parsed_page.css('div.row.result')
+  url_init = "https://www.indeed.com/jobs?q=software+engineer+javascript&l=San+Francisco,+CA&explvl=entry_level&start=10"
+  unparsed_page_init = HTTParty.get(url_init)
+  parsed_page_init = Nokogiri::HTML(unparsed_page_init)
+  job_listings_init = parsed_page_init.css('div.row.result')
 
   jobs = []
-  page = 0
-  per_page = job_listings.count
-  total_text = parsed_page.css('div#searchCount')[0].children.text.chomp.strip
+  page = 10
+  per_page = job_listings_init.count
+  total_text = parsed_page_init.css('div#searchCount')[0].children.text.chomp.strip
   total = /(\d+).(\d+)/.match(total_text)[0].to_i
-  last_page = total/10
 
-  while page < last_page
-    url = "https://www.indeed.com/jobs?q=software+engineer+javascript&l=San+Francisco,+CA&explvl=entry_level&start=#{page}"
-    unparsed_page = HTTParty.get(url)
+  while page < total + 10
+    page_url = "https://www.indeed.com/jobs?q=software+engineer+javascript&l=San+Francisco,+CA&explvl=entry_level&start=#{page}"
+    puts "URL: #{page_url}"
+    unparsed_page = HTTParty.get(page_url)
     parsed_page = Nokogiri::HTML(unparsed_page)
     job_listings = parsed_page.css('div.row.result') #16 jobs
 
     job_listings.each do |job_listing|
       url = ""
-      if !job_listing.css('a.jobtitle').empty?
+      if job_listing.css('a.jobtitle').text.chomp.strip != ""
         url = job_listing.css('a.jobtitle')[0].attributes["href"].value
       end
 
-      job = {
-        title: job_listing.css('a.jobtitle').text,
-        company: job_listing.css('span.company').text.chomp.strip,
-        location: job_listing.css('span.location').text,
-        url: "https://www.indeed.com" + url,
-        summary: job_listing.css('span.summary'),
-        reqs: job_listing.css('span.experienceList'),
-      }
-      jobs << job
-      byebug
+      if url != ""
+        job = {
+          title: job_listing.css('a.jobtitle').text,
+          company: job_listing.css('span.company').text.chomp.strip,
+          location: job_listing.css('span.location').text,
+          url: "https://www.indeed.com" + url,
+          summary: job_listing.css('span.summary'),
+          reqs: job_listing.css('span.experienceList'),
+        }
+        jobs << job
+        puts "Added #{job[:title]}"
+
+      end
     end
     page += 10
   end
+  byebug
 end
 
 scraper
